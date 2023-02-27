4380d2c445eecfdb0255b8b6027df751620c43d5
Jack Shih <randomdize@gmail.com>
2023-02-27 13:54:04 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A flake for Hello World";
+
+  # NOTE(Jack): Using default nixpkgs instead.
+  # inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-linux =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerication {
+        name = "hello";
+        src = self;
+        buildPhase = "gcc -o hello ./hello.c";
+        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+      };
+  };
+    defaultPackage.aarch64-darwin =
+      with import nixpkgs { system = "aarch64-darwin"; };
+      stdenv.mkDerivation {
+        name = "hello";
+        src = self;
+        # NOTE(Jack): macOS doesn't have gcc.
+        buildInputs = [
+          gcc
+        ];
+        buildPhase = "gcc -o hello ./hello.c";
+        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+      };
+  };
+}
